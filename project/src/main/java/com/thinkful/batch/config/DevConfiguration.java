f23999a0a1d6faaf30b7e85602d42d1d0da6ec8d
neville <combat>
2015-04-29 05:38:43 +0200

    WIP

---
+++ project/src/main/java/com/thinkful/batch/config/DevConfiguration.java
@@ -0,0 +1,7 @@
+package com.thinkful.batch.config;
+
+/**
+ * Created by neville on 29/04/15.
+ */
+public class DevConfiguration {
+}
---
+++ project/src/main/java/com/thinkful/batch/config/ProdConfiguration.java
@@ -0,0 +1,12 @@
+package com.thinkful.batch.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Created by neville on 29/04/15.
+ */
+@Profile()
+@Configuration
+public class ProdConfiguration {
+}
