a588e8d839b8b444c1373424fbac8ace3a670b51
neville <combat>
2015-05-14 22:21:01 +0200

    Included datetime in the vehicle dto.

--- contract/src/main/java/com/thinkful/contract/dto/VehicleDto.java
+++ contract/src/main/java/com/thinkful/contract/dto/VehicleDto.java
@@ -1,10 +1,12 @@
 package com.thinkful.contract.dto;
 
 import lombok.Data;
+import org.joda.time.DateTime;
 
 @Data
 public class VehicleDto {
     private Long id;
     private VehicleModelDto model;
     private String color;
+    private DateTime date = DateTime.now();
 }
